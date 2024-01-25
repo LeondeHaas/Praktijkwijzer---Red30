5959dc52cb24314336220db459c9e67d252a3199
xLeonPlays <1195150@student.roc-nijmegen.nl>
2024-01-25 13:16:52 +0100

    Homepage button gaat naar register pagina

--- red30/src/App.jsx
+++ red30/src/App.jsx
@@ -2,25 +2,27 @@ import AuthDetails from "./components/auth/AuthDetails";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
 import Header from "./components/Header";
-import Fuitmand from "./imgs/fruithartje.webp"
+import Fuitmand from "./imgs/fruithartje.webp";
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    <Header />
-    <div className="welcome-section">
-      <h1>
-        Welkom bij <span className="red-word">Red</span>30
-      </h1>
-      <p className="motto-text">Our motto is Love your body and it will love you back</p>
-      <button className="get-started-button">Get Started Today</button>
+      <Header />
+      <div className="welcome-section">
+        <h1>
+          Welkom bij <span className="red-word">Red</span>30
+        </h1>
+        <p className="motto-text">Our motto is Love your body and it will love you back</p>
+        <Link to="/register">
+          <button className="get-started-button">Get Started Today</button>
+        </Link>
+      </div>
+      <div className="fruitmanddoos">
+        <img src={Fuitmand} alt="Fruitmand" className="fruit-image" />
+      </div>
     </div>
-    <div className="fruitmanddoos">
-    <img src={Fuitmand} alt="Fruitmand" className="fruit-image" />
-    </div>
-  </div>
-);
+  );
 };
-
 
 export default App;
